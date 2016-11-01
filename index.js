e5ea30e3de48b49d378ce06152417ab36a661665
amareedunaway <amaree.dunaway.2018@gmail.com>
2016-11-01 17:43:00 +0000

    I'm ready to commit my files.

---
+++ index.js
@@ -0,0 +1,38 @@
+var add = function (a,b) {
+return a + b;
+};
+
+console.log (add(5,4))
+
+var subtract = function (a,b) {
+  return a - b;
+};
+
+console.log (subtract(5,4))
+
+var multiply = function (a,b) {
+  return a * b;
+};
+
+console.log (multiply(5,4))
+
+var divide = function (a,b) {
+return a / b;
+};
+console.log (divide(5,4))
+
+var inc = function(n) {
+  return n +1;
+};
+
+var dec = function (n) {
+  return n-1;
+};
+
+var makeInt = function(string) {
+  return parseInt (string, 10);
+};
+
+var preserveDecimal = function (decimal) {
+  return parseFloat (decimal, 10);
+};
